13bbaf6cfbd58defccc03f990cd5d7c28e94cf31
Rodrigo Cavalcante <rodrigocavalcante.pro@gmail.com>
2023-05-16 11:41:57 -0300

    handle with ui logic updates based on media player

--- app/src/main/java/com/rodcollab/lingoleap/search/SearchScreen.kt
+++ app/src/main/java/com/rodcollab/lingoleap/search/SearchScreen.kt
@@ -2,51 +2,92 @@ package com.rodcollab.lingoleap.search
 
 import android.media.AudioAttributes
 import android.media.MediaPlayer
-import android.net.Uri
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewmodel.compose.*
 import com.rodcollab.lingoleap.R
+import kotlinx.coroutines.*
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SearchScreen(modifier: Modifier, viewModel: SearchViewModel = viewModel()) {
-
-    val context = LocalContext.current
-
-
-    val uri = Uri.parse("https://ssl.gstatic.com/dictionary/static/sounds/20200429/hello--_gb_1.mp3")
-
-    val mediaPlayer = remember { MediaPlayer() }
     // on below line we are creating a variable for our audio url
-    var audioUrl = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+
+
+    val audioUrl = "https://ssl.gstatic.com/dictionary/static/sounds/20200429/hello--_gb_1.mp3"
 
     val audioAttributes = AudioAttributes.Builder()
         .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
         .setUsage(AudioAttributes.USAGE_MEDIA)
         .build()
 
-    mediaPlayer.setAudioAttributes(audioAttributes)
+    val mediaPlayer by remember { mutableStateOf(MediaPlayer()) }
 
-    var isPlaying = remember { mutableStateOf(false) }
+    var progress by remember { mutableStateOf(0f) }
+
+    var isPlaying by remember { mutableStateOf(false) }
+
+    DisposableEffect(audioUrl) {
+
+
+        mediaPlayer.apply {
+            setDataSource(audioUrl)
+            setAudioAttributes(audioAttributes)
+            prepare()
+        }
+
+        onDispose {
+            mediaPlayer.release()
+
+        }
+    }
+    DisposableEffect(Unit) {
+
+        isPlaying = mediaPlayer.isPlaying
+
+        onDispose {
+            isPlaying = false
+        }
+    }
+
+    if (isPlaying) {
+        LaunchedEffect(Unit) {
+            while (true) {
+
+                progress = mediaPlayer.currentPosition.toFloat()
+                delay(1000 / 30)
+
+            }
+        }
+    }
+
+
+    LaunchedEffect(mediaPlayer) {
+
+        mediaPlayer.setOnCompletionListener {
+
+            progress = 0f
+            isPlaying = false
+
+        }
+    }
+
 
     val openDialog = remember { mutableStateOf(false) }
-    var progress = remember { mutableStateOf(0f) }
+
 
     val state = viewModel.state
     val keyBoardController = LocalSoftwareKeyboardController.current
@@ -99,54 +140,35 @@ fun SearchScreen(modifier: Modifier, viewModel: SearchViewModel = viewModel()) {
                     ) {
                         IconButton(
                             onClick = {
+                                Log.d("isPlaying", isPlaying.toString())
 
-                                // on below line we are running a try and catch block
-                                // for our media player.
-                                try {
-                                    // on below line we are setting audio source
-                                    // as audio url on below line.
-                                    mediaPlayer.setDataSource(audioUrl)
-
-                                    // on below line we are preparing
-                                    // our media player.
-                                    mediaPlayer.prepare()
-
-
-                                    progress.value = (mediaPlayer.currentPosition / 100).toFloat()
-
-                                    // on below line we are starting
-                                    // our media player.
-                                    if(mediaPlayer.isPlaying) {
-                                        mediaPlayer.stop()
-                                    } else {
-                                        mediaPlayer.start()
-                                    }
-
-
-                                } catch (e: Exception) {
-
-                                    // on below line we are
-                                    // handling our exception.
-                                    e.printStackTrace()
+                                isPlaying = if (isPlaying) {
+                                    mediaPlayer.pause()
+                                    false
+                                } else {
+                                    mediaPlayer.start()
+                                    true
                                 }
+
                             }
                         ) {
                             Icon(
-                                painter = painterResource(id = setIcon(isPlaying.value)),
+                                painter = painterResource(id = setIcon(isPlaying)),
                                 contentDescription = "Play",
                                 tint = MaterialTheme.colors.primary
                             )
                         }
 
                         Slider(
-                            value = progress.value,
-                            onValueChange = { progress.value = it },
+                            value = progress,
+                            onValueChange = { progress = it },
                             modifier = Modifier
                                 .fillMaxWidth()
                                 .padding(horizontal = 16.dp),
                             colors = SliderDefaults.colors(
                                 thumbColor = MaterialTheme.colors.primary
-                            )
+                            ),
+                            valueRange = 0f..mediaPlayer.duration.toFloat()
                         )
                     }
                 }
@@ -156,15 +178,9 @@ fun SearchScreen(modifier: Modifier, viewModel: SearchViewModel = viewModel()) {
 }
 
 fun setIcon(isPlaying: Boolean): Int {
-    return if(isPlaying) {
+    return if (isPlaying) {
         R.drawable.ic_pause
     } else {
         R.drawable.ic_play
     }
-}
-
-@Composable
-fun DialogWindow() {
-
-
 }
