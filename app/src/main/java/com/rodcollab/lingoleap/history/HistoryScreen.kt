5cbf66b5667f148526ad2b1d0f36452c1ab2dad0
Rodrigo Cavalcante <rodrigocavalcante.pro@gmail.com>
2023-05-17 22:36:59 -0300

    add navigation to the project

---
+++ app/src/main/java/com/rodcollab/lingoleap/history/HistoryScreen.kt
@@ -0,0 +1,15 @@
+package com.rodcollab.lingoleap.history
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HistoryScreen(modifier: Modifier) {
+
+    Column() {
+        Text("History Screen")
+    }
+
+}
