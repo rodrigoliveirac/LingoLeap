59a0c1a03df23a4bf29b678acca3be134e69849c
Rodrigo Cavalcante <rodrigocavalcante.pro@gmail.com>
2023-08-10 23:21:53 -0300

    creating translator service, create a instance and so on

---
+++ app/src/main/java/com/rodcollab/lingoleap/di/TranslatorApiModule.kt
@@ -0,0 +1,43 @@
+package com.rodcollab.lingoleap.di
+
+import com.rodcollab.lingoleap.ApiKeyInterceptor
+import com.rodcollab.lingoleap.TranslatorApiService
+import com.rodcollab.lingoleap.TranslatorApiService.Companion.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TranslatorApiModule {
+
+    @Provides
+    @Singleton
+    fun provideApiKeyInterceptor(): ApiKeyInterceptor {
+        return ApiKeyInterceptor("1cd6ae4891msh4f3856046fcb584p1dcc2fjsna733a41907bd")
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(apiKeyInterceptor: ApiKeyInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(apiKeyInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOpenAIApi(okHttpClient: OkHttpClient): TranslatorApiService {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(TranslatorApiService.moshi))
+            .build()
+            .create(TranslatorApiService::class.java)
+    }
+}
