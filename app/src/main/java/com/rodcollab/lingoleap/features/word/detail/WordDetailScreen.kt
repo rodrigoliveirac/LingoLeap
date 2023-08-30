9d075f7bdd5c6ec6cf5962c7492ac0c11c353b0f
Rodrigo Cavalcante <rodrigocavalcante.pro@gmail.com>
2023-08-30 01:44:33 -0300

    refactoring

--- app/src/main/java/com/rodcollab/lingoleap/features/word/detail/WordDetailScreen.kt
+++ app/src/main/java/com/rodcollab/lingoleap/features/word/detail/WordDetailScreen.kt
@@ -1,76 +1,52 @@
 package com.rodcollab.lingoleap.features.word.detail
 
+import androidx.compose.animation.core.Spring.DampingRatioMediumBouncy
+import androidx.compose.animation.core.Spring.StiffnessLow
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.ArrowDropUp
-import androidx.compose.material.icons.filled.VolumeUp
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.lifecycleScope
 import com.rodcollab.lingoleap.R
-import com.rodcollab.lingoleap.features.word.translation.TranslateComponent
-import com.rodcollab.lingoleap.features.word.translation.ItemComponent
+import com.rodcollab.lingoleap.features.word.detail.components.MainWordDetailsPagerComponent
+import com.rodcollab.lingoleap.features.word.detail.components.SongsWordDetailsPagerComponent
 import java.util.*
 
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun WordDetailScreen(
+    hideNavBar: () -> Unit,
     onNavigateBack: () -> Unit,
     wordDetailsViewModel: WordDetailsViewModel = hiltViewModel()
 ) {
 
+    DisposableEffect(Unit) {
+        hideNavBar()
+        onDispose { }
+    }
+
     val wordDetailsUiState by wordDetailsViewModel.word.collectAsState()
-    val languages by wordDetailsViewModel.languages.collectAsState()
 
-    var actualPartOfSpeech by rememberSaveable { mutableStateOf(wordDetailsUiState.partOfSpeech) }
-
-    var translatedText by rememberSaveable { mutableStateOf("") }
-
-    var isLoading by rememberSaveable { mutableStateOf(false) }
-
-    var selectedLanguage by remember { mutableStateOf<(String?) -> Unit>({}) }
-
-    var langCode by remember { mutableStateOf(Locale.getDefault().language) }
-
-    var wordTranslated by remember { mutableStateOf("") }
-
-    var isWordTranslated by remember { mutableStateOf(false) }
-
-    val scope = LocalLifecycleOwner.current.lifecycleScope
+    val pagerState = rememberPagerState(pageCount = {
+        2
+    })
 
     DisposableEffect(Unit) {
-//        scope.launch {
-//            wordDetailsViewModel.translate(langCode = langCode, wordDetailsViewModel.wordId) {
-//                wordTranslated = it
-//                isWordTranslated = true
-//            }
-//        }
+        wordDetailsViewModel.getSongs()
         onDispose { }
     }
 
@@ -78,30 +54,23 @@ fun WordDetailScreen(
         wordDetailsViewModel.loadLanguages()
     }
 
-    /*selectedLanguage = { selectedLang ->
-        isLoading = true
-        if (selectedLang != null) {
-            langCode = selectedLang
-            scope.launch {
-                scope.launch {
-                    *//*wordDetailsViewModel.translate(langCode, actualText) { newTranslatedText ->
-                        translatedText = newTranslatedText
-                        isLoading = false
-                    }*//*
-                }
-                scope.launch {
-                    wordDetailsViewModel.translate(
-                        langCode = langCode,
-                        wordDetailsViewModel.wordId
-                    ) {
-                        wordTranslated = it
-                        isWordTranslated = true
-                        isLoading = false
-                    }
-                }
-            }
+    var scroll by remember { mutableStateOf(false) }
+
+    var goTo by remember { mutableStateOf(false) }
+
+    LaunchedEffect(scroll) {
+        if (goTo) {
+            pagerState.animateScrollToPage(
+                page = 1,
+                animationSpec = spring(
+                    dampingRatio = DampingRatioMediumBouncy,
+                    stiffness = StiffnessLow
+                )
+            )
+            goTo = false
         }
-    }*/
+    }
+
 
     Scaffold(topBar = {
         TopAppBar() {
@@ -113,234 +82,32 @@ fun WordDetailScreen(
             Text(text = stringResource(R.string.word_details_title))
         }
     }) {
-        Box(modifier = Modifier.padding(it)) {
-            Column(
-                modifier = Modifier.padding(
-                    top = 32.dp,
-                    start = 16.dp,
-                    end = 16.dp,
-                    bottom = 32.dp
-                )
-            ) {
-
-
-                Row {
-                    Text(
-                        modifier = Modifier.padding(start = 12.dp),
-                        fontSize = 40.sp,
-                        fontWeight = FontWeight.Bold,
-                        text = wordDetailsUiState.word
-                    )
-                    Box {
-                        if (isLoading && !isWordTranslated) {
-                            CircularProgressIndicator(
-                                strokeWidth = 1.dp,
-                                strokeCap = StrokeCap.Square,
-                                color = Color(255, 20, 147),
-                                modifier = Modifier
-                                    .align(Alignment.Center)
-                                    .size(72.dp)
-                                    .padding(24.dp)
-                            )
-                        }
-                    }
-
-                }
-
-                LazyRow(
-                    modifier = Modifier
-                        .padding(start = 12.dp, top = 16.dp, end = 12.dp)
-                ) {
-                    items(wordDetailsUiState.partOfSpeeches) { partOfSpeech ->
-                        Text(modifier = Modifier
-                            .clickable {
-                                wordDetailsViewModel.getDefinitionsBy(partOfSpeech)
-                                actualPartOfSpeech = partOfSpeech
-                            }
-                            .border(
-                                1.dp,
-                                if (wordDetailsUiState.partOfSpeech == partOfSpeech) Color(
-                                    250,
-                                    128,
-                                    46
-                                ) else Color.LightGray,
-                                RoundedCornerShape(2.dp)
-                            )
-                            .padding(start = 6.dp, top = 2.dp, bottom = 2.dp, end = 6.dp),
-                            text = partOfSpeech,
-                            fontSize = 14.sp)
-
-                        Spacer(modifier = Modifier.size(8.dp))
-                    }
-                }
-
-                IconButton(onClick = { }) {
-                    Icon(
-                        modifier = Modifier
-                            .padding(top = 16.dp, bottom = 24.dp),
-                        imageVector = Icons.Default.VolumeUp,
-                        contentDescription = null
-                    )
-                }
-
-                Text(
-                    color = Color.DarkGray,
-                    modifier = Modifier
-                        .align(Alignment.Start)
-                        .padding(start = 12.dp, bottom = 8.dp),
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.Light,
-                    text = "meanings"
-                )
-
-                LazyColumn(modifier = Modifier.fillMaxSize()) {
-
-                    items(wordDetailsUiState.definitionsAndExamples) { item ->
-                        MeaningItemComponent(
-                            isLoading,
-                            item.definition,
-                            item.example
-                        )
-                    }
-                }
-            }
-        }
-    }
-}
-
-@Composable
-@OptIn(ExperimentalFoundationApi::class)
-private fun MeaningItemComponent(
-    isLoading: Boolean,
-    definition: String,
-    example: String
-) {
-
-    val state = rememberPagerState(pageCount = {
-        2
-    })
-
-    var actualPage by rememberSaveable { mutableStateOf(2) }
-
-    var arrowClicked by remember { mutableStateOf(false) }
-
-    var showTranslationComponent by remember { mutableStateOf(false) }
-
-    LaunchedEffect(arrowClicked) {
-        showTranslationComponent = arrowClicked
-    }
-
-    LaunchedEffect(state) {
-        snapshotFlow { state.currentPage }.collect { page ->
-            actualPage = page
-        }
-    }
-
-    Card(
-        backgroundColor = Color(255, 255, 255, 255),
-        shape = RoundedCornerShape(12.dp),
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 12.dp, top = 16.dp, end = 12.dp, bottom = 8.dp),
-        elevation = 4.dp
-    ) {
-        Column(
+        Box(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(top = 24.dp, start = 16.dp, end = 16.dp, bottom = 16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .padding(it)
         ) {
-
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Spacer(
-                    modifier = Modifier
-                        .align(Alignment.Bottom)
-                        .weight(1f)
-                        .height(1.dp)
-                        .background(Color.LightGray)
-                )
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth(0.5f)
-                        .bottomBorder(
-                            1.dp,
-                            if (actualPage == 0) Color(250, 128, 46) else Color.LightGray
-                        )
-                        .padding(bottom = 8.dp), text = "Definition"
-                )
-                Spacer(
-                    modifier = Modifier
-                        .align(Alignment.Bottom)
-                        .weight(1f)
-                        .height(1.dp)
-                        .background(Color.LightGray)
-                )
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .bottomBorder(
-                            1.dp,
-                            if (actualPage == 1) Color(250, 128, 46) else Color.LightGray
-                        )
-                        .padding(bottom = 8.dp), text = "Sentences"
-                )
-                Spacer(
-                    modifier = Modifier
-                        .align(Alignment.Bottom)
-                        .weight(1f)
-                        .height(1.dp)
-                        .background(Color.LightGray)
-                )
-            }
-
-            HorizontalPager(
-                state = state,
-                beyondBoundsPageCount = 2,
-            ) { page ->
+            VerticalPager(state = pagerState) { page ->
                 when (page) {
                     0 -> {
-                        DefinitionsPage(isLoading = isLoading, definition = definition)
+                        MainWordDetailsPagerComponent(
+                            toSongsPager = {
+                                scroll = true
+                                goTo = true
+                            },
+                            wordDetailsUiState,
+                            wordDetailsViewModel,
+                        )
                     }
 
-                    1 -> SentencesPage(isLoading = isLoading, sentence = example)
+                    1 -> {
+                        SongsWordDetailsPagerComponent(wordDetailsUiState)
+                        scroll = false
+                    }
                 }
             }
 
-            IconButton(
-                modifier = Modifier
-                    .align(Alignment.End)
-                    .padding(top = 16.dp),
-                onClick = { arrowClicked = !arrowClicked }) {
-                Icon(
-                    imageVector = if (showTranslationComponent) Icons.Filled.ArrowDropUp else Icons.Filled.ArrowDropDown,
-                    contentDescription = null,
-                    modifier = Modifier.size(32.dp)
-                )
-            }
-            if (arrowClicked) {
-                Text(
-                    modifier = Modifier
-                        .align(Alignment.CenterHorizontally),
-                    text = "Translate to"
-                )
-                TranslateComponent({ }, emptyList(), isLoading, definition)
-            }
         }
     }
-}
-
-@Composable
-fun SentencesPage(isLoading: Boolean, sentence: String) {
-    ItemComponent(isLoading = isLoading, text = sentence)
-}
-
-@Composable
-fun DefinitionsPage(isLoading: Boolean, definition: String) {
-    ItemComponent(isLoading = isLoading, text = definition)
 }
 
 fun Modifier.bottomBorder(strokeWidth: Dp, color: Color) = composed(
@@ -361,9 +128,3 @@ fun Modifier.bottomBorder(strokeWidth: Dp, color: Color) = composed(
         }
     }
 )
-
-@Preview
-@Composable
-fun PagerScreenPreview() {
-
-}
--- app/src/main/java/com/rodcollab/lingoleap/features/word/translation/ItemComponent.kt
+++ app/src/main/java/com/rodcollab/lingoleap/features/word/translation/ItemComponent.kt
@@ -3,8 +3,6 @@ package com.rodcollab.lingoleap.features.word.translation
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material3.Card
@@ -14,26 +12,29 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun ItemComponent(isLoading: Boolean, text: String) {
+fun ItemComponent(text: String) {
 
     var expanded by remember { mutableStateOf(false) }
 
-    Card(modifier = Modifier
-        .fillMaxWidth()
-        .padding(start = 16.dp, top = 24.dp,bottom = 8.dp, end = 16.dp), shape = MaterialTheme.shapes.medium,
-        elevation = CardDefaults.cardElevation(5.dp)) {
-        if(isLoading) {
-            CircularProgressIndicator(strokeWidth = 1.dp, strokeCap = StrokeCap.Square, color = Color(255,20,147), modifier = Modifier.size(72.dp).align(Alignment.CenterHorizontally).padding(24.dp))
-        } else {
-            Text(modifier = Modifier.clickable { expanded = !expanded }.padding(16.dp),text = text, maxLines = if(expanded) Int.MAX_VALUE else 1, overflow = if(expanded) TextOverflow.Visible else TextOverflow.Ellipsis)
-        }
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, top = 24.dp, bottom = 8.dp, end = 16.dp),
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(5.dp)
+    ) {
+        Text(
+            modifier = Modifier
+                .clickable { expanded = !expanded }
+                .padding(16.dp),
+            text = text,
+            maxLines = if (expanded) Int.MAX_VALUE else 1,
+            overflow = if (expanded) TextOverflow.Visible else TextOverflow.Ellipsis
+        )
     }
 }
